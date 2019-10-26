0e8a9c11176ae0ca674dfdbd7dc9ae888d9c3c59
dinotuku <tungkuan0528@gmail.com>
2019-10-26 18:39:45 +0200

    Add Kuan's version of submission

--- proj1_helpers.py
+++ proj1_helpers.py
@@ -4,6 +4,7 @@
 
 import csv
 import numpy as np
+from activations import sigmoid
 
 
 def load_csv_data(data_path, sub_sample=False):
@@ -15,7 +16,7 @@ def load_csv_data(data_path, sub_sample=False):
 
     # convert class labels from strings to binary (-1,1)
     yb = np.ones(len(y))
-    yb[np.where(y=='b')] = -1
+    yb[np.where(y == 'b')] = -1
 
     # sub-sample
     if sub_sample:
@@ -35,6 +36,15 @@ def predict_labels(weights, data):
     return y_pred
 
 
+def predict_lg_labels(weights, data):
+    """Generates class predictions given weights, and a test data matrix"""
+    y_pred = sigmoid(np.dot(data, weights))
+    y_pred[np.where(y_pred <= 0.5)] = 0
+    y_pred[np.where(y_pred > 0.5)] = 1
+
+    return y_pred
+
+
 def create_csv_submission(ids, y_pred, name):
     """
     Creates an output file in csv format for submission to kaggle
@@ -47,4 +57,103 @@ def create_csv_submission(ids, y_pred, name):
         writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
-            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+            writer.writerow({'Id': int(r1), 'Prediction': int(r2)})
+
+
+def train_val_split(y, tx, val_per, seed=1):
+    """Split data set into training set and validation set"""
+    np.random.seed(seed)
+    total_num = len(y)
+    val_num = int(total_num * val_per)
+    indices = np.random.permutation(total_num)
+    val_idx, train_idx = indices[:val_num], indices[val_num:]
+    return y[train_idx], y[val_idx], tx[train_idx, :], tx[val_idx, :]
+
+
+def build_k_indices(y, k_fold, seed):
+    """Build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
+
+
+def compute_accuracy(y_true, y_pred):
+    """Compute accuracy"""
+    return sum(y_true == y_pred) / len(y_true)
+
+
+def compute_ls_loss(y, tx, w):
+    """Compute mean squared error"""
+    e = y - tx.dot(w)
+    loss = 1 / 2 * np.mean(e**2)
+    return loss
+
+
+def compute_ls_gradient(y, tx, w):
+    """Compute the gradient and loss."""
+    e = y - tx.dot(w)
+    grad = -1 / len(e) * tx.T.dot(e)
+    return grad
+
+
+def compute_lg_loss(y, tx, w):
+    """Compute the cost by negative log likelihood."""
+    pred = sigmoid(tx.dot(w))
+    loss = y.T.dot(np.log(pred)) + (1 - y).T.dot(np.log(1 - pred))
+    return np.squeeze(-loss) / y.shape[0]
+
+
+def compute_lg_gradient(y, tx, w):
+    """Compute the gradient of loss."""
+    pred = sigmoid(tx.dot(w))
+    grad = tx.T.dot(pred - y)
+    return grad / y.shape[0]
+
+
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def nn_batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """Generate a minibatch iterator for a dataset."""
+    data_size = y.shape[1]
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[:, shuffle_indices]
+        shuffled_tx = tx[:, shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[:, start_index:end_index], shuffled_tx[:, start_index:end_index]
--- run.py
+++ run.py
@@ -1,11 +1,65 @@
 # -*- coding: utf-8 -*-
 
-"""reproduce our best submission"""
+"""
+ML project 1 - Spot the Boson
+This will produce exactly the same .csv predictions
+which we used in our best submission to the competition system.
+"""
+
+import numpy as np
+from nn import *
+from proj1_helpers import *
 
 
 def main():
     """ Main function """
-    raise NotImplementedError
+    # read training and testing data
+    DATA_TRAIN_PATH = '../../data/train.csv'
+    DATA_TEST_PATH = '../../data/test.csv'
+    y, x, _ = load_csv_data(DATA_TRAIN_PATH)
+    _, x_test, ids_test = load_csv_data(DATA_TEST_PATH)
+
+    # split training data into training and validation
+    y_train, y_val, x_train, x_val = train_val_split(y, x, 0.2, seed=1)
+
+    # normalize data using metrics of training data
+    # (except PRI_jet_num (22th column) since it is a discrete value)
+    nor_indices = [idx for idx in range(x_train.shape[1]) if idx != 22]
+    nor_x_train = x_train.copy()
+    nor_x_val = x_val.copy()
+    nor_x_test = x_test.copy()
+
+    x_train_mean = x_train[:, nor_indices].mean(axis=0)
+    x_train_std = x_train[:, nor_indices].std(axis=0)
+
+    nor_x_train[:, nor_indices] = (nor_x_train[:, nor_indices] - x_train_mean) / x_train_std
+    nor_x_val[:, nor_indices] = (nor_x_val[:, nor_indices] - x_train_mean) / x_train_std
+    nor_x_test[:, nor_indices] = (nor_x_test[:, nor_indices] - x_train_mean) / x_train_std
+
+    # change y label from (1, -1) to (1, 0)
+    # since the output of sigmoid is between (1, 0)
+    nn_y_train = np.where(y_train == -1, 0, y_train)
+    nn_y_val = np.where(y_val == -1, 0, y_val)
+
+    # set parameters
+    epochs = 100
+    lr = 0.001
+    batch_size = 250
+
+    # train model, get model parameters
+    nn_params = train(nor_x_train.T, nn_y_train[np.newaxis,:], nor_x_val.T, nn_y_val[np.newaxis,:],
+                    NN_ARCHITECTURE, epochs, lr, batch_size, verbose=True)
+
+    # read the best model in terms of accuracy and get the output of it
+    nn_params = np.load('best_acc.npy', allow_pickle=True).item()
+    y_test_hat, _ = full_forward_propagation(nor_x_test.T, nn_params, NN_ARCHITECTURE)
+    # get the prediction labels
+    y_test_pred = np.where(np.squeeze(y_test_hat.T) > 0.5, 1, -1)
+
+    # save our prediction
+    OUTPUT_PATH = 'test.csv'
+    create_csv_submission(ids_test, y_test_pred, OUTPUT_PATH)
+
 
 if __name__ == '__main__':
 
