76d8cf0661c0878a967aa2408274b565985a5045
劉德翎 <louis0939194682@gmail.com>
2019-10-27 18:59:14 +0100

    update

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-ML project 1 - Spot the Boson
-This will produce exactly the same .csv predictions
-which we used in our best submission to the competition system.
+Main File for ML project 1
+Spot the Boson
+This will produce the .csv predictions which we used
+in our best submission to the competition system.
 """
 
 import numpy as np
@@ -13,44 +14,19 @@ from proj1_helpers import *
 
 def main():
     """ Main function """
-    # read training and testing data
-    DATA_TRAIN_PATH = '../../data/train.csv'
-    DATA_TEST_PATH = '../../data/test.csv'
-    y, x, _ = load_csv_data(DATA_TRAIN_PATH)
+    # read testing data
+    DATA_TEST_PATH = './data/test.csv'
     _, x_test, ids_test = load_csv_data(DATA_TEST_PATH)
+    train_data_metrics = np.load('train_data_metrics.npy', allow_pickle=True).item()
 
-    # split training data into training and validation
-    y_train, y_val, x_train, x_val = train_val_split(y, x, 0.2, seed=1)
-
-    # normalize data using metrics of training data
-    # (except PRI_jet_num (22th column) since it is a discrete value)
-    nor_indices = [idx for idx in range(x_train.shape[1]) if idx != 22]
-    nor_x_train = x_train.copy()
-    nor_x_val = x_val.copy()
+    # normalize testing data with training data mean and std
+    x_train_mean = train_data_metrics['mean']
+    x_train_std = train_data_metrics['std']
+    nor_indices = [idx for idx in range(x_test.shape[1]) if idx != 22]
     nor_x_test = x_test.copy()
-
-    x_train_mean = x_train[:, nor_indices].mean(axis=0)
-    x_train_std = x_train[:, nor_indices].std(axis=0)
-
-    nor_x_train[:, nor_indices] = (nor_x_train[:, nor_indices] - x_train_mean) / x_train_std
-    nor_x_val[:, nor_indices] = (nor_x_val[:, nor_indices] - x_train_mean) / x_train_std
     nor_x_test[:, nor_indices] = (nor_x_test[:, nor_indices] - x_train_mean) / x_train_std
 
-    # change y label from (1, -1) to (1, 0)
-    # since the output of sigmoid is between (1, 0)
-    nn_y_train = np.where(y_train == -1, 0, y_train)
-    nn_y_val = np.where(y_val == -1, 0, y_val)
-
-    # set parameters
-    epochs = 100
-    lr = 0.001
-    batch_size = 250
-
-    # train model, get model parameters
-    nn_params = train(nor_x_train.T, nn_y_train[np.newaxis,:], nor_x_val.T, nn_y_val[np.newaxis,:],
-                    NN_ARCHITECTURE, epochs, lr, batch_size, verbose=True)
-
-    # read the best model in terms of accuracy and get the output of it
+    # read the best model and get the output of it
     nn_params = np.load('best_acc.npy', allow_pickle=True).item()
     y_test_hat, _ = full_forward_propagation(nor_x_test.T, nn_params, NN_ARCHITECTURE)
     # get the prediction labels
