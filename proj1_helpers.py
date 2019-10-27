0c81a88f646af83ad3477d39ec658dacfdc834a8
De-Ling Liu <32357494+louis0939@users.noreply.github.com>
2019-10-27 23:35:04 +0100

    Update proj1_helpers.py

--- proj1_helpers.py
+++ proj1_helpers.py
@@ -222,3 +222,157 @@ def load_test_data_split(DATA_TEST_PATH):
     x_1 = standardize(x_1)
     x_2 = standardize(x_2)
     return x_0, x_1, x_2, id_0, id_1, id_2
+
+def build_k_indices(y, k_fold, seed):
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval:(k + 1) * interval]
+                    for k in range(k_fold)]
+    return np.array(k_indices)
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression."""
+    L = 2 * lambda_ * tx.shape[0] * np.identity(tx.shape[1])
+    a = np.linalg.inv(np.dot(np.transpose(tx), tx) + L)
+    b = np.dot(np.transpose(tx), y)
+    # ridge regression: TODO
+    return np.dot(a, b)
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    poly = np.ones((len(x), 1))
+    for deg in range(1, degree+1):
+        poly = np.c_[poly, np.power(x, deg)]
+    return poly
+
+
+def sigmoid(t):
+    sigma = 1/(1+np.exp(-t))
+    return sigma
+
+
+def calculate_loss(y, tx, w):
+    loss = y.T.dot(tx.dot(w)) - np.sum(np.log(1 + np.exp(tx.dot(w))))
+    return -loss
+
+
+def calculate_gradient(y, tx, w):
+    sigma = sigmoid(tx.dot(w))
+    grad = tx.T.dot(sigma-y)
+    return grad
+
+
+def penalized_logistic_regression(y, tx, w, lambda_):
+    loss = calculate_loss(y, tx, w) + lambda_ * w.T.dot(w)
+    grad = calculate_gradient(y, tx, w) + 2 * lambda_ * w
+    return loss, grad
+
+
+def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
+    loss, grad = penalized_logistic_regression(y, tx, w, lambda_)
+    w = w - gamma * grad
+    return loss, w
+
+
+def cross_validation(y, x, index, k, lambda_, degree):
+    x_tr = x.drop(x.index[index[k]])
+    y_tr = np.delete(y, index[k])
+    x_te = x.iloc[index[k]]
+    y_te = y[index[k]]
+    
+    y_tr = np.asarray(y_tr).reshape((y_tr.shape[0], 1))
+    tx = build_poly(x_tr, degree)
+    tx_te = build_poly(x_te, degree)
+    
+    _, w = logistic_regression_penalized_gradient_descent(y_tr, x_tr, lambda_, degree)
+    
+    return calculate_loss(y_te, tx_te, w), w
+
+
+def cross_validation_select_lambda(x, y, degree, k_fold, seed = 1):
+    lambdas = np.logspace(-6,-3, 4)
+    lambdas = np.insert(lambdas,0,0)
+    index = build_k_indices(y, k_fold, seed)
+    loss = []
+    
+    for lambda_ in lambdas:
+        print(lambda_)
+        loss_tmp = []
+        for k in range(k_fold):
+            print(k)
+            losses,_ = cross_validation(y, x, index, k, lambda_, degree)
+            loss_tmp.append(losses)
+            print(loss_tmp)
+        loss.append(np.mean(loss_tmp))
+        print(loss)
+        
+    ind_lambda_opt = np.argmin(loss)
+    return lambdas[ind_lambda_opt]
+
+def LG_load_train_data_split(DATA_TRAIN_PATH):
+    
+    train = pd.read_csv(DATA_TRAIN_PATH)
+    train['Prednum'] = np.where(train['Prediction'] == 's', 0, 1)
+    
+    x_9 = train[train['DER_mass_MMC'] == -999].drop(columns=['Id', 'Prediction','DER_mass_MMC','Prednum'])    
+    x_0 = train[(train['PRI_jet_num'] == 0) & (train['DER_mass_MMC'] != -999)].drop(columns=['Id', 'Prediction','PRI_jet_num',
+                                                             'DER_deltaeta_jet_jet','DER_mass_jet_jet',
+                                                             'DER_prodeta_jet_jet', 'DER_lep_eta_centrality',
+                                                             'PRI_jet_leading_pt','PRI_jet_leading_eta',
+                                                             'PRI_jet_leading_phi', 'PRI_jet_subleading_pt',
+                                                             'PRI_jet_subleading_eta','PRI_jet_subleading_phi',
+                                                             'PRI_jet_all_pt','Prednum'])
+    
+    x_1 = train[(train['PRI_jet_num'] == 1) & (train['DER_mass_MMC'] != -999)].drop(columns=['Id', 'Prediction', 'PRI_jet_num',
+                                                             'DER_deltaeta_jet_jet','DER_mass_jet_jet',
+                                                             'DER_prodeta_jet_jet','DER_lep_eta_centrality',
+                                                             'PRI_jet_subleading_pt','PRI_jet_subleading_eta',
+                                                             'PRI_jet_subleading_phi','PRI_jet_all_pt','Prednum'])
+    
+    x_2 = train[((train['PRI_jet_num'] == 2) | (train['PRI_jet_num'] == 3)) & (train['DER_mass_MMC'] != -999)].drop(columns=['Id', 'Prediction', 'PRI_jet_num','Prednum'])
+     
+    y = train[['Prednum', 'PRI_jet_num', 'DER_mass_MMC']]
+    y_0 = y[(y['PRI_jet_num'] == 0) & (y['DER_mass_MMC'] != -999)]['Prednum'].values
+    y_1 = y[(y['PRI_jet_num'] == 1) & (y['DER_mass_MMC'] != -999)]['Prednum'].values
+    y_2 = y[((y['PRI_jet_num'] == 2) | (y['PRI_jet_num'] == 3)) & (y['DER_mass_MMC'] != -999)]['Prednum'].values
+    y_9 = y[y['DER_mass_MMC'] == -999]['Prednum'].values
+    
+    x_0 = standardize(x_0)
+    x_1 = standardize(x_1)
+    x_2 = standardize(x_2)
+    x_9 = standardize(x_9)
+    
+    return x_0, x_1, x_2, x_9, y_0, y_1, y_2, y_9
+
+def LG_load_test_data_split(DATA_TEST_PATH):
+    test = pd.read_csv(DATA_TEST_PATH)
+    id_9 = test[test['DER_mass_MMC'] == -999]['Id']
+    id_0 = test[(test['PRI_jet_num'] == 0) & (test['DER_mass_MMC'] != -999)]["Id"]
+    id_1 = test[(test['PRI_jet_num'] == 1) & (test['DER_mass_MMC'] != -999)]["Id"]
+    id_2 = test[((test['PRI_jet_num'] == 2) | (test['PRI_jet_num'] == 3)) & (test['DER_mass_MMC'] != -999)]["Id"]
+    
+    x_9 = test[test['DER_mass_MMC'] == -999].drop(columns=['Id', 'Prediction','DER_mass_MMC'])
+    x_0 = test[(test['PRI_jet_num'] == 0) & (test['DER_mass_MMC'] != -999)].drop(columns=['Id', 'Prediction', 'PRI_jet_num',
+                                                             'DER_deltaeta_jet_jet','DER_mass_jet_jet',
+                                                             'DER_prodeta_jet_jet', 'DER_lep_eta_centrality',
+                                                             'PRI_jet_leading_pt','PRI_jet_leading_eta',
+                                                             'PRI_jet_leading_phi', 'PRI_jet_subleading_pt',
+                                                             'PRI_jet_subleading_eta','PRI_jet_subleading_phi',
+                                                             'PRI_jet_all_pt'])
+    x_1 = test[(test['PRI_jet_num'] == 1) & (test['DER_mass_MMC'] != -999)].drop(columns=['Id', 'Prediction', 'PRI_jet_num',
+                                                             'DER_deltaeta_jet_jet','DER_mass_jet_jet',
+                                                             'DER_prodeta_jet_jet','DER_lep_eta_centrality',
+                                                             'PRI_jet_subleading_pt','PRI_jet_subleading_eta',
+                                                             'PRI_jet_subleading_phi','PRI_jet_all_pt'])
+    
+    x_2 = test[((test['PRI_jet_num'] == 2) | (test['PRI_jet_num'] == 3)) & (test['DER_mass_MMC'] != -999)].drop(columns=['Id','Prediction', 'PRI_jet_num'])
+    
+    x_0 = standardize(x_0)
+    x_1 = standardize(x_1)
+    x_2 = standardize(x_2)
+    x_9 = standardize(x_9)
+    return x_0, x_1, x_2, x_9, id_0, id_1, id_2, id_9
